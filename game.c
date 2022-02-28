ca2ae0255819c3675d070233a9ebad5db8824846
Mey Corentin <corentin.mey@epitech.eu>
2022-02-28 16:05:03 +0100

    [V1.3] +add ground

--- game.c
+++ game.c
@@ -80,6 +80,25 @@ void place_struct(global *game, struct grid_cell *new, shop *my_shop)
         place_struct(game, new->next_cell, my_shop);
 }
 
+void add_ground(global *game , struct grid_cell *new, shop * my_shop)
+{
+    if(new->status == 1) {
+        sfSprite *ground_struct = sfSprite_create();
+        sfTexture *ground_struct_text = sfTexture_createFromFile("pictures/maps/ground/struct_ground.png", sfFalse);
+        sfVector2f ground_size = {0.8, 0.7};
+        sfVector2f ground_pos;
+
+        ground_pos.x = new->p_3.x - 60 + 10;
+        ground_pos.y = new->p_3.y - (abs(new->dy) + abs(new->dy) + abs(new->dy)) + 29;
+        sfSprite_setScale(ground_struct, ground_size);
+        sfSprite_setTexture(ground_struct, ground_struct_text, sfFalse);
+        sfSprite_setPosition(ground_struct, ground_pos);
+        sfRenderWindow_drawSprite(game->window, ground_struct, sfFalse);
+    }
+    if (new->g_pos != 196)
+        add_ground(game, new->next_cell, my_shop);
+}
+
 void start_game(global *game)
 {
     shop *my_shop = create_shop();
@@ -99,6 +118,7 @@ void start_game(global *game)
         update_game(game);                                                    // Met a jour les sprites.
         draw_game(game, my_shop);                                             // Draw la map.
         add_cell_status(game, grid_cell.next_cell, my_shop);                  // Modifie le status des cellules.
+        add_ground(game, grid_cell.next_cell, my_shop);
         (game->god == 0) ? draw_cell(game, grid_cell.next_cell, my_shop) : 1; // Affiche les cellules.
         sfRenderWindow_drawCircleShape(game->window , game->radius , sfFalse);
         place_struct(game, &grid_cell, my_shop);                              // Place tous les bâtiments.
